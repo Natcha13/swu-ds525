f0e8e697d2947a8404cf52c7cd3d4d225bbb2c99
Natcha Chinnapan <natchachinnapan@gmail.com>
2022-09-22 17:19:03 +0000

    final

--- 01-data-modeling-i/create_tables.py
+++ 01-data-modeling-i/create_tables.py
@@ -8,52 +8,47 @@ table_drop_event   = "DROP TABLE IF EXISTS Event;"
 
 table_create_actor = """
     CREATE TABLE IF NOT EXISTS Actor (
-        actor_id BIGINT NOT NULL,
+        actor_id INTEGER PRIMARY KEY,
         actor_login VARCHAR(50) NOT NULL,
         actor_display_login VARCHAR(50) NOT NULL,
         actor_gravatar_id VARCHAR(50),
         actor_url VARCHAR(100) NOT NULL,
-        actor_avatar_url VARCHAR(100) NOT NULL,
-        PRIMARY KEY (actor_id)
+        actor_avatar_url VARCHAR(100) NOT NULL
     );
 """
 table_create_repo = """
     CREATE TABLE IF NOT EXISTS Repo (
-        repo_id BIGINT NOT NULL,
+        repo_id INTEGER PRIMARY KEY,
         repo_name VARCHAR(100) NOT NULL,
-        repo_url VARCHAR(150) NOT NULL,
-        PRIMARY KEY (repo_id)
+        repo_url VARCHAR(150) NOT NULL
     );
 """
 table_create_payload = """
     CREATE TABLE IF NOT EXISTS Payload (
-        payload_push_id BIGINT NOT NULL,
-        payload_size BIGINT NOT NULL,
-        payload_ref VARCHAR(200) NOT NULL,
-        PRIMARY KEY (payload_push_id)
+        payload_push_id INTEGER PRIMARY KEY,
+        payload_size INTEGER NOT NULL,
+        payload_ref VARCHAR(200) NOT NULL
     );
 """
 table_create_org = """
     CREATE TABLE IF NOT EXISTS Org (
-        org_id BIGINT NOT NULL,
+        org_id INTEGER PRIMARY KEY,
         org_login VARCHAR(50) NOT NULL,
-        org_gravatar_id VARCHAR(50),
-        org_url VARCHAR(100) NOT NULL,
-        org_avatar_url VARCHAR(100) NOT NULL,
-        PRIMARY KEY (org_id)
+        org_gravatar_id VARCHAR(100),
+        org_url VARCHAR(255) NOT NULL,
+        org_avatar_url VARCHAR(255) NOT NULL
     );
 """
 table_create_event = """
     CREATE TABLE IF NOT EXISTS Event (
-        event_id VARCHAR(20) NOT NULL,
+        event_id INTEGER PRIMARY KEY,
         event_type VARCHAR(50) NOT NULL,
         event_public BOOLEAN NOT NULL,
         event_created_at TIMESTAMP NOT NULL,
-        event_repo_id BIGINT NOT NULL,
-        event_actor_id BIGINT NOT NULL,
-        event_org_id BIGINT,
-        event_payload_push_id BIGINT,
-        PRIMARY KEY (event_id),
+        event_repo_id INTEGER NOT NULL,
+        event_actor_id INTEGER NOT NULL,
+        event_org_id INTEGER,
+        event_payload_push_id INTEGER,
         FOREIGN KEY (event_repo_id)     REFERENCES Repo     (repo_id),
         FOREIGN KEY (event_actor_id)    REFERENCES Actor    (actor_id),
         FOREIGN KEY (event_org_id)      REFERENCES Org      (org_id),
